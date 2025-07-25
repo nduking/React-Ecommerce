96ff7db2bf98337332f82cc98ab09643ea83be58
nduking <114291880+nduking@users.noreply.github.com>
2025-07-25 14:16:19 +0100

    Herocat responsiveness

--- src/Components/Herocat.jsx
+++ src/Components/Herocat.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FaChevronRight } from "react-icons/fa";
 const Herocat = () => {
   return (
-    <section className="py-24 bg-[url(/furniturebg.svg)] px-28">
+    <section className="py-24 bg-[url(/furniturebg.svg)] lg:px-28 px-12 md:px-18">
       <div className="flex items-center py-4 text-furnituregrey">
         <span className="flex items-center gap-2">
           Home <FaChevronRight />
         </span>{" "}
         <span>New Luma Yoga Collection</span>
       </div>
-      <h1 className="text-4xl text-furnituregrey">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl text-furnituregrey">
         Simple Wood Chair Collection
       </h1>
     </section>
